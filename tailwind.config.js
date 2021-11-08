885d6d933f9b11894bce78396bfa3f40c72d2fed
gn-grc <genejogarcia.gg@gmail.com>
2021-11-08 15:34:25 +0800

    changed tailwind default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      serif: ["Bitter", "serif"],
+    },
     extend: {},
   },
   variants: {
