2b4c7f53d24c2d87c6c0cecf3b90c17855423723
gn-grc <genejogarcia.gg@gmail.com>
2021-11-08 16:11:41 +0800

    color palette changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,22 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      black: "#212427",
+    },
     fontFamily: {
       sans: ["Open Sans", "sans-serif"],
       serif: ["Bitter", "serif"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "body-color": "#292929",
+        accent: "#2196F3",
+        "my-green": "#16C79A",
+        "my-orange": "#EE6F57",
+        "my-purple": "#A82FFC",
+      },
+    },
   },
   variants: {
     extend: {},
