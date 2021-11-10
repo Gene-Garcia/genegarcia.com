cc4a5ca3b2ad4fc049d8aef8514242d927180db6
gn-grc <genejogarcia.gg@gmail.com>
2021-11-10 20:54:07 +0800

    added initial data of portfolio projects

--- src/screens/App/screens/Portfolio/utils/data.js
+++ src/screens/App/screens/Portfolio/utils/data.js
@@ -1,8 +1,143 @@
-export default {
-  KEY: {
-    name: "",
-    logo: "LOC",
+import lokalLogo from "../../../../../shared/images/project_logos/lokal.png";
+import mclLogo from "../../../../../shared/images/project_logos/mcl.png";
+import floresDeMayoIs from "../../../../../shared/images/project_logos/floresdemayo-mis.png";
+import tutoQuizzer from "../../../../../shared/images/project_logos/tutoquizzer.png";
+import sinisianBMIS from "../../../../../shared/images/project_logos/sinisian-bmis.png";
 
+export default {
+  LOKAL: {
+    name: "Lokal Music",
+    logo: lokalLogo,
+
+    date: "November 2020 - February 2021",
+    description:
+      "An eCommerce website and a platform for digital music created by Filipino artists.",
+    technologies: [
+      "ASP .NET",
+      "MVC",
+      "SQL Server",
+      "Entity Framework",
+      "C#",
+      "HTML",
+      "CSS",
+      "Bootstrap",
+    ],
+    github: false,
+
+    live: false,
+    link: "",
+
+    gallery: ["LOC1", "LOC2"],
+  },
+
+  MCL_SCHOLAR_WEBSITE: {
+    name: "MCL Scholar Website",
+    logo: mclLogo,
+
+    date: "February - March 2019",
+    description:
+      "A web application for MCL students and scholarship admins to improve certain scholarship areas.",
+    technologies: [
+      "ASP .NET",
+      "MVC",
+      "SQL Server",
+      "Entity Framework",
+      "Identity Framework",
+      "C#",
+      "HTML",
+      "CSS",
+      "Bootstrap",
+    ],
+    github: false,
+
+    live: false,
+    link: "",
+
+    gallery: ["LOC1", "LOC2"],
+  },
+
+  FLORES_DE_MAYO_IS: {
+    name: "Flores De Mayo Information System",
+    logo: floresDeMayoIs,
+
+    date: "February 2018",
+    description:
+      "An information system website designed to support the management of the Alayan Organization records.",
+    technologies: [
+      "ASP .NET",
+      "MVC",
+      "SQL Server",
+      "Entity Framework",
+      "Identity Framework",
+      "C#",
+      "HTML",
+      "CSS",
+      "Bootstrap",
+    ],
+    github: "https://github.com/Gene-Garcia/Flores-De-Mayo-IS",
+
+    live: false,
+    link: "",
+
+    gallery: ["LOC1", "LOC2"],
+  },
+
+  TUTOQUIZZER: {
+    name: "TutoQuizzer",
+    logo: tutoQuizzer,
+
+    date: "March 2018",
+    description:
+      "An android application that aims to assist students in reviewing and retaining learning concepts.",
+    technologies: [
+      "Android",
+      "Room Database",
+      "SQLite",
+      "Java",
+      "Local Storage",
+    ],
+    github: "https://github.com/Gene-Garcia/TutoQuizzer",
+
+    live: false,
+    link: "",
+
+    gallery: ["LOC1", "LOC2"],
+  },
+
+  SINISIAN_BARANGAY_MIS: {
+    name: "Sinisian Barangay Management Information System",
+    logo: sinisianBMIS,
+
+    date: "May 2017",
+    description:
+      "A web-based information system for Barangay Sinisian that digitilizes the record management of residents.",
+    technologies: [
+      "ASP .NET",
+      "MVC",
+      "SQL Server",
+      "Entity Framework",
+      "Identity Framework",
+      "C#",
+      "HTML",
+      "CSS",
+      "Bootstrap",
+    ],
+    github: "",
+
+    live: false,
+    link: "",
+
+    gallery: ["LOC1", "LOC2"],
+  },
+};
+
+/*
+
+KEY: {
+    name: "",
+    logo: "../../../../../shared/images/project_logos/",
+
+    date: "",
     description: "",
     technologies: ["KEY1", "KEY2"],
     github: "",
@@ -12,4 +147,5 @@ export default {
 
     gallery: ["LOC1", "LOC2"],
   },
-};
+
+ */
