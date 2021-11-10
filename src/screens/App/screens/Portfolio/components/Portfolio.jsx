f6d150906ee593401b8b0db1d3f76e0ab9351748
gn-grc <genejogarcia.gg@gmail.com>
2021-11-10 21:46:44 +0800

    implemented rendering of all project data

--- src/screens/App/screens/Portfolio/components/Portfolio.jsx
+++ src/screens/App/screens/Portfolio/components/Portfolio.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useEffect } from "react/cjs/react.development";
 import useNavbar from "../../../../../context/useNavbar";
+import Container from "../../../../../shared/components/Container";
+import Heading from "../../../../../shared/components/Heading";
+import { ProjectCard } from "../utils/Cards";
+
+// project data
+import data from "../utils/data";
 
 function Portfolio() {
   // navbar context
@@ -12,9 +18,15 @@ function Portfolio() {
   }, []);
 
   return (
-    <>
-      <h1>Portfolio</h1>
-    </>
+    <Container>
+      <Heading head="Portfolio" />
+
+      <div className="my-10 flex flex-col gap-6">
+        {Object.entries(data).map(([k, v]) => (
+          <ProjectCard key={k} data={v} />
+        ))}
+      </div>
+    </Container>
   );
 }
 
