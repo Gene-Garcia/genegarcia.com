296077a7dbed48dede3d11439ba2c661ec0189f8
gn-grc <genejogarcia.gg@gmail.com>
2021-11-10 21:46:23 +0800

    added responsive x axis margin of container

--- src/shared/components/Container.jsx
+++ src/shared/components/Container.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Container({ children }) {
-  return <div className="my-12 mx-36">{children}</div>;
+  return (
+    <div className="py-12 mx-8 sm:mx-14 lg:mx-20 xl:mx-32 2xl:mx-36">
+      {children}
+    </div>
+  );
 }
 
 export default Container;
