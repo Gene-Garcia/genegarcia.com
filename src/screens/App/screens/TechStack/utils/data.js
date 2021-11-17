f71fc5537a0c22cefd0ed35b2547e50439719f35
gn-grc <genejogarcia.gg@gmail.com>
2021-11-17 22:51:26 +0800

    added tech stack data

---
+++ src/screens/App/screens/TechStack/utils/data.js
@@ -0,0 +1,107 @@
+export default {
+  WEB_APPLICATION: {
+    title: "Web Application",
+
+    groups: [
+      {
+        title: "MERN Full Stack",
+        technology: ["MONGO_DB", "EXPRESS", "REACT", "NODE_JS"],
+      },
+      {
+        title: "Mark Language and Design",
+        technology: ["HTML_5", "CSS_3", "BOOTSTRAP"],
+      },
+      {
+        title: "Scripting Languages",
+        technology: ["JAVASCRIPT"],
+      },
+      {
+        title: "Server-Side",
+        technology: ["JAVASCRIPT", "C_SHARP", "PYTHON"],
+      },
+      {
+        title: "Frameworks",
+        technology: [
+          "ASP_DOT_NET",
+          "MONGOOSE",
+          "FLASK",
+          "ENTITY",
+          "IDENTITY",
+          "MVC_ARCH",
+        ],
+      },
+    ],
+  },
+
+  SMARTPHONE: {
+    title: "Smartphone",
+
+    groups: [
+      {
+        title: "Programming Language",
+        technology: ["ANDROID", "JAVA"],
+      },
+      {
+        title: "Database",
+        technology: ["SQL", "SQL_LITE"],
+      },
+      {
+        title: "Frameworks",
+        technology: ["ROOM_DB", "MVVP_ARCH"],
+      },
+    ],
+  },
+
+  DESKTOP: {
+    title: "Desktop",
+
+    groups: [
+      {
+        title: "Programming Language",
+        technology: ["C_SHARP"],
+      },
+      {
+        title: "Database",
+        technology: ["SQL", "SQL_SERVER"],
+      },
+      {
+        title: "Framework",
+        technology: ["MVP_ARCH", "WINDOWS_FORMS"],
+      },
+    ],
+  },
+
+  SERVICES: {
+    title: "Services",
+
+    groups: [
+      {
+        title: "Version Control",
+        technology: ["GITHUB"],
+      },
+      {
+        title: "Database",
+        technology: ["MONGO_DB", "AZURE_DB"],
+      },
+      {
+        title: "Deployment",
+        technology: [
+          "GITHUB_PAGES",
+          "AZURE",
+          "HEROKU",
+          "NETLIFY",
+          "HEROKU",
+          "NAMECHEAP",
+        ],
+      },
+      {
+        title: "Design",
+        technology: ["FIGMA"],
+      },
+      {
+        title: "IDE",
+        technology: ["VISUAL_STUDIO", "VS_CODE"],
+      },
+    ],
+  },
+};
