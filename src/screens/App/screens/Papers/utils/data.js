e4edb6c48907ae4a03b863bdfa6e7b2895339aa6
gn-grc <genejogarcia.gg@gmail.com>
2021-11-22 21:21:55 +0800

    added initial data of papers

---
+++ src/screens/App/screens/Papers/utils/data.js
@@ -0,0 +1,50 @@
+export default {
+  INCIDENT_REPORTING: {
+    date: "June 2021",
+    title:
+      "Designing an Incident Reporting Web Application for Sinisian, Calaca, Batangas",
+
+    paperType: "",
+    doi: "",
+    link: "",
+    location: "",
+    abstract: "",
+  },
+
+  COVID_FMIS: {
+    date: "April 2021",
+    title:
+      "Impact of the COVID-19 pandemic to Financial Management Information System and Crisis Management",
+
+    paperType: "CONFERENCE PAPER",
+    doi: "10.14458/RSU.res.2021.173",
+    link: "https://rsucon.rsu.ac.th/files/proceedings/intersoc2021/2107_20210513190115.pdf",
+    location: "2021 Internatial RSU Research Conference",
+    abstract:
+      "The COVID-19 pandemic posed a great threat, not only to the health of the people but also to the health of the economy and businesses. The objectives of this paper are to review and determine the impact of the COVID-19 pandemic on the economy and organization’s operation. Additionally, it also aims to identify action plans that support the recovery from the pandemic. This paper used a narrative literature review as a research methodology to gather timely and relevant resources, which draw insights and knowledge from the literature. The underlying factors of the COVID-19 pandemic, specifically, government measures, are causing the disruption and changes in the economic activity of every country and business. It was observed that the government measures, as a response to the pandemic, consequently resulted in the decline of businesses’ financial performance through low consumer demands, buying power, and limited resources. For instance, limiting the outdoor movements and access to non-essentials, as government response, consequently resulted in the decline of businesses’ sales profit and revenues. Some organizations, however, implemented crisis management protocols and policies to survive and recover from the COVID-19 pandemic. A Study about the businesses’ performance during this pandemic identified models and constructs related to survivability and resilience as crisis management. This paper then summarized the information into a crisis management model that is seen to improve the performance and recovery of the businesses from the impact of the pandemic. Additionally, this paper also introduced a model of the COVID-19 impact that was identified in the organizations’ experiences. These models can provide relevant information about the pandemic and initiate preparedness. Furthermore, contribute support to the businesses’ crisis management initiatives to thrive and recover from the hostile business and financial environment brought by crises, such as this COVID-19 pandemic.",
+  },
+
+  BURGER_KING: {
+    date: "July 202",
+    title:
+      "An Analysis of the Major Business Process of an Organization: The Case of Burger King Corporation",
+
+    paperType: "",
+    doi: "",
+    link: "",
+    location: "",
+    abstract: "",
+  },
+
+  WALMART: {
+    date: "February 2020",
+    title:
+      "Information Systems Utilization of an Organization: The Case of Walmart Inc.",
+
+    paperType: "",
+    doi: "",
+    link: "",
+    location: "",
+    abstract: "",
+  },
+};
