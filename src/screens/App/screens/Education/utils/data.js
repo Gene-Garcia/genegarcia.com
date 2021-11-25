8fc43ca541815fbe283a6c00645e44016e6d7cf1
gn-grc <genejogarcia.gg@gmail.com>
2021-11-25 14:49:41 +0800

    added school data

---
+++ src/screens/App/screens/Education/utils/data.js
@@ -0,0 +1,57 @@
+// image import
+import cms from "../../../../../shared/images/schools/cms.webp";
+import mcl from "../../../../../shared/images/schools/mcl.webp";
+
+export default {
+  COLLEGE: {
+    school: "Malayan Colleges Laguna",
+    specialization: "BS Information Systems",
+    logo: mcl,
+
+    awards: {
+      FRESH_SOPH: {
+        name: "Freshman & Sophomore",
+        list: ["Dean's Lister", "President's Lister", "Full Academic Scholar"],
+      },
+    },
+
+    linkName: "mcl.edu.ph",
+    link: "https://mcl.edu.ph",
+  },
+
+  SHS: {
+    school: "Malayan Colleges Laguna",
+    specialization: "Information & Communications Technology - Programming",
+    logo: mcl,
+
+    awards: {
+      YEAR: {
+        name: "2017 - 2019",
+        list: [
+          "Graduated with Honors",
+          "Best in Work Immersion",
+          "codeX Challenge Champion",
+        ],
+      },
+    },
+
+    linkName: "mcl.edu.ph",
+    link: "https://mcl.edu.ph",
+  },
+
+  JHS: {
+    school: "Creative Middle School",
+    specialization: "Junior High School",
+    logo: cms,
+
+    awards: {
+      YEAR: {
+        name: "2013 - 2017",
+        list: ["Graduated with Honors"],
+      },
+    },
+
+    linkName: "cms.edu.ph",
+    link: "https://mcl.edu.ph",
+  },
+};
