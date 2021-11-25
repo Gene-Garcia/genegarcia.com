13a6da68b523851be2f19184100b8f8b184b50da
gn-grc <genejogarcia.gg@gmail.com>
2021-11-25 15:45:39 +0800

    updated container

--- src/shared/components/Container.jsx
+++ src/shared/components/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Container({ children }) {
   return (
-    <div className="py-12 mx-8 sm:mx-14 lg:mx-20 xl:mx-32 2xl:mx-36">
+    <div className="py-6 pb-12 mx-6 sm:mx-16 lg:mx-20 xl:mx-32 2xl:mx-36">
       {children}
     </div>
   );
