35af7fe0ac07bf8c92f48dd4a657fb9a6942fc36
gn-grc <genejogarcia.gg@gmail.com>
2022-01-15 21:33:38 +0800

    changed nav G logo image

--- src/screens/App/components/Nav.jsx
+++ src/screens/App/components/Nav.jsx
@@ -6,7 +6,7 @@ import useWindow from "../../../hooks/useWindow";
 import routes from "../../../shared/routes";
 
 // Icon Logo
-import imageLogo from "../../../shared/images/logov2.png";
+import imageLogo from "../../../shared/images/LogoV3.2.png";
 
 // Text Logo
 import textLogo from "../../../shared/images/logonamev1.png";
@@ -35,7 +35,7 @@ function Responsive({ context: linksState }) {
     <div className="flex flex-row justify-between items-center gap-x-0 px-3 md:px-6 h-ten">
       {/* logo */}
       <div className="flex flex-row flex-shrink items-center">
-        <img src={imageLogo} className="w-10 h-11" alt="G" />
+        <img src={imageLogo} className="w-8" alt="G" />
         {/* <img src={textLogo} className="h-8 w-auto" /> */}
       </div>
 
@@ -68,30 +68,30 @@ function Collapsible({ context: linksState }) {
 
   return (
     <div className="">
-      <div className="p-3">
+      <div className="p-4">
         <button onClick={toggleNav}>
-          <img src={imageLogo} className="w-10 h-11" alt="G" />
+          <img src={imageLogo} className="w-8" alt="G" />
         </button>
       </div>
 
       <div
         className={`${
           toggled ? "fixed" : "hidden"
-        } z-50 p-3 bg-gray-200 w-48 left-0 inset-y-0 h-screen`}
+        } z-50 p-4 bg-gray-200 w-48 left-0 inset-y-0 h-screen`}
       >
         <button onClick={toggleNav}>
-          <img src={imageLogo} className="w-10 h-11" alt="G" />
+          <img src={imageLogo} className="w-8" alt="G" />
         </button>
 
         {/* link */}
-        <div className="my-14 flex flex-col space-y-8">
+        <div className="my-12 flex flex-col space-y-7">
           {Object.entries(routes).map(([k, v]) => (
             <Link
               key={k}
               to={v.PATH}
               className={`${
-                linksState[k] ? "text-accent" : "text-black"
-              } font-sans font-semibold text-sm transition duration-200 hover:text-accent `}
+                linksState[k] ? "text-accent" : "text-gray-600"
+              } font-sans font-medium text-base transition duration-200 hover:text-accent `}
             >
               {v.NAME.toUpperCase()}
             </Link>
