5d04de29d3354a6b7e0b9d84d76a33a0d6e0417d
gn-grc <genejogarcia.gg@gmail.com>
2022-03-13 22:54:19 +0800

    added initial template for portfolio data

---
+++ src/screens/App/screens/AcademicPortfolio/utils/data.js
@@ -0,0 +1,73 @@
+const academicPortData = {
+  FRESHMAN: {
+    NAME: "Freshman",
+    TERM1: [
+      {
+        COURSE_NAME: "Lorem Ipsum",
+        COURSE_CODE: "LI1-12",
+        PROFESSOR: "John Doe",
+
+        TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
+
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "#",
+        FILE_LINK: "#",
+      },
+      {
+        COURSE_NAME: "Lorem Ipsum",
+        COURSE_CODE: "LI1-12",
+        PROFESSOR: "John Doe",
+
+        TYPES: ["RESEARCH", "PROGRAMMING"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "#",
+        FILE_LINK: false,
+      },
+    ],
+    TERM2: [],
+    TERM3: [],
+  },
+
+  SOPHOMORE: {
+    NAME: "Sophomore",
+    TERM1: [],
+    TERM2: [],
+    TERM3: [],
+  },
+
+  JUNIOR: {
+    NAME: "Junior",
+    TERM1: [],
+    TERM2: [],
+    // TERM3: [],
+  },
+
+  //   SENIOR: {
+  //     NAME: "SENIOR",
+  //     TERM1: [],
+  //     TERM2: [],
+  //     TERM3: [],
+  //   },
+};
+
+const a = {
+  COURSE_NAME: "",
+  COURSE_CODE: "",
+  PROFESSOR: "",
+
+  TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
+  PROJECT_NAME: "Lorem ipsum",
+  PROJECT_DESCRIPTION:
+    "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+  LIVE_LINK: "",
+  FILE_LINK: "",
+};
+
+export default academicPortData;
