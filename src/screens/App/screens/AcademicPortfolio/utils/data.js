10a17d60b210821f4a70517926093f604a84aec2
gn-grc <genejogarcia.gg@gmail.com>
2022-03-14 11:25:12 +0800

    added course codes and course name for academic portfolio

--- src/screens/App/screens/AcademicPortfolio/utils/data.js
+++ src/screens/App/screens/AcademicPortfolio/utils/data.js
@@ -3,63 +3,59 @@ const academicPortData = {
     NAME: "Freshman",
     T1: [
       {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
+        COURSE_NAME: "Introduction to Computer Systems",
+        COURSE_CODE: "CS100",
+        PROFESSOR: "Adomar Ilao",
 
-        TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
-
+        TYPES: ["CASE STUDY"],
         PROJECT_NAME: "Lorem ipsum",
-        PROJECT_DESCRIPTION:
-          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+        PROJECT_DESCRIPTION: "Suspendisse maximus pharetra augue id porta.",
 
-        LIVE_LINK: "#",
-        FILE_LINK: "#",
-      },
-      {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
-
-        TYPES: ["RESEARCH", "PROGRAMMING"],
-        PROJECT_NAME: "Lorem ipsum",
-        PROJECT_DESCRIPTION:
-          "Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
-
-        LIVE_LINK: "#",
-        FILE_LINK: false,
+        LIVE_LINK: "",
+        FILE_LINK: "",
       },
     ],
     T2: [
       {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
+        COURSE_NAME: "Fundamentals of Information Systems",
+        COURSE_CODE: "IS100",
+        PROFESSOR: "Ellenita Red",
 
-        TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
-
+        TYPES: ["CASE STUDY"],
         PROJECT_NAME: "Lorem ipsum",
         PROJECT_DESCRIPTION:
           "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
 
-        LIVE_LINK: "#",
-        FILE_LINK: "#",
+        LIVE_LINK: "",
+        FILE_LINK: "",
       },
     ],
     T3: [
       {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
+        COURSE_NAME: "Business Processes",
+        COURSE_CODE: "IS111",
+        PROFESSOR: "Ellenita Red",
 
-        TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
-
+        TYPES: ["CASE STUDY"],
         PROJECT_NAME: "Lorem ipsum",
         PROJECT_DESCRIPTION:
           "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
 
-        LIVE_LINK: "#",
-        FILE_LINK: "#",
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Computer Programming Concepts",
+        COURSE_CODE: "IT101-2 & IT101-2L",
+        PROFESSOR: "Rhea Tortor",
+
+        TYPES: ["PROGRAMMING"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
       },
     ],
   },
@@ -68,50 +64,138 @@ const academicPortData = {
     NAME: "Sophomore",
     T1: [
       {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
+        COURSE_NAME: "Data Structures and Algorithms",
+        COURSE_CODE: "CS102-1 & CS102-1L",
+        PROFESSOR: "Khristian Kikuchi & Rhea Tortor",
 
         TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
-
         PROJECT_NAME: "Lorem ipsum",
         PROJECT_DESCRIPTION:
           "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
 
-        LIVE_LINK: "#",
-        FILE_LINK: "#",
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Enterprise Systems",
+        COURSE_CODE: "IS109",
+        PROFESSOR: "Ellenita Red",
+
+        TYPES: ["CASE STUDY"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Concepts in Object Oriented Programming",
+        COURSE_CODE: "IT11 & IT11L",
+        PROFESSOR: "Adomar Ilao",
+
+        TYPES: ["PROGRAMMING"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
       },
     ],
     T2: [
       {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
+        COURSE_NAME: "Information Management",
+        COURSE_CODE: "CS107 & CS107L",
+        PROFESSOR: "Adomar Ilao",
 
-        TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
-
+        TYPES: ["RESEARCH", "PROGRAMMING"],
         PROJECT_NAME: "Lorem ipsum",
         PROJECT_DESCRIPTION:
           "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
 
-        LIVE_LINK: "#",
-        FILE_LINK: "#",
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Web Systems and Technologies",
+        COURSE_CODE: "IT114 & IT114L",
+        PROFESSOR: "Adomar Ilao",
+
+        TYPES: ["RESEARCH", "PROGRAMMING"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Financial Management for Information Systems",
+        COURSE_CODE: "IS116",
+        PROFESSOR: "Ellenita Red",
+
+        TYPES: ["RESEARCH", "CASE STUDY"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Financial Management for Information Systems Laboratory",
+        COURSE_CODE: "IS116L",
+        PROFESSOR: "Ellenita Red",
+
+        TYPES: ["RESEARCH", "PRACTICAL"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
       },
     ],
     T3: [
       {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
+        COURSE_NAME: "System Analysis and Design",
+        COURSE_CODE: "CS103P",
+        PROFESSOR: "Adomar Ilao",
 
-        TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
-
+        TYPES: ["RESEARCH", "PROGRAMMING"],
         PROJECT_NAME: "Lorem ipsum",
         PROJECT_DESCRIPTION:
           "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
 
-        LIVE_LINK: "#",
-        FILE_LINK: "#",
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Business Intelligence",
+        COURSE_CODE: "IS113",
+        PROFESSOR: "Ellenita Red",
+
+        TYPES: ["RESEARCH"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Information Systems Innovation and New Technologies",
+        COURSE_CODE: "IS114",
+        PROFESSOR: "Ellenita Red",
+
+        TYPES: ["RESEARCH", "CASE STUDY"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
       },
     ],
   },
@@ -120,34 +204,45 @@ const academicPortData = {
     NAME: "Junior",
     T1: [
       {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
+        COURSE_NAME: "Information Systems Project Management 1",
+        COURSE_CODE: "IS131-1P",
+        PROFESSOR: "Adomar Ilao",
 
-        TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
-
+        TYPES: ["RESEARCH"],
         PROJECT_NAME: "Lorem ipsum",
         PROJECT_DESCRIPTION:
           "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
 
-        LIVE_LINK: "#",
-        FILE_LINK: "#",
+        LIVE_LINK: "",
+        FILE_LINK: "",
+      },
+      {
+        COURSE_NAME: "Enterprise Resource Planning",
+        COURSE_CODE: "IS136P",
+        PROFESSOR: "Adomar Ilao",
+
+        TYPES: ["PRACTICAL"],
+        PROJECT_NAME: "Lorem ipsum",
+        PROJECT_DESCRIPTION:
+          "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
+
+        LIVE_LINK: "",
+        FILE_LINK: "",
       },
     ],
     T2: [
       {
-        COURSE_NAME: "Lorem Ipsum",
-        COURSE_CODE: "LI1-12",
-        PROFESSOR: "John Doe",
+        COURSE_NAME: "Information Systems Project Management 2",
+        COURSE_CODE: "IS131-2P",
+        PROFESSOR: "Adomar Ilao",
 
-        TYPES: ["RESEARCH", "PROGRAMMING", "CASE STUDY"],
-
+        TYPES: ["RESEARCH"],
         PROJECT_NAME: "Lorem ipsum",
         PROJECT_DESCRIPTION:
           "Suspendisse maximus pharetra augue id porta. Donec eu enim justo. Maecenas nec ante non nibh mattis ornare. Nulla facilisi. Nullam id aliquam eros. Nullam sit amet sapien eget augue semper ornare. Nullam ligula nisi, sollicitudin vitae suscipit a, congue porta dui. Mauris tincidunt metus orci, et ultricies dui blandit id.",
 
-        LIVE_LINK: "#",
-        FILE_LINK: "#",
+        LIVE_LINK: "",
+        FILE_LINK: "",
       },
     ],
     // TERM3: [],
