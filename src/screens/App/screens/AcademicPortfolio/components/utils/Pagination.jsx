94714e4c326b7ea8341c28db7c0a47bbf2ba88de
gn-grc <genejogarcia.gg@gmail.com>
2022-03-14 14:46:42 +0800

    tweaked pagination colors

--- src/screens/App/screens/AcademicPortfolio/components/utils/Pagination.jsx
+++ src/screens/App/screens/AcademicPortfolio/components/utils/Pagination.jsx
@@ -11,10 +11,10 @@ const Pagination = () => {
         <button
           onClick={toggleThisYear}
           className={`bg-white w-32 h-7
-          rounded outline outline-accent
+          rounded-bl-lg rounded-xs rounded-tr-lg outline outline-accent
           text-sm font-medium text-accent
           transition duration-150 ease-linear
-          hover:bg-accent hover:text-white
+          hover:bg-accent hover:text-white hover:bg-opacity-70
           ${thisYear && "ring-1 ring-accent ring-offset-2"}`}
         >
           {YEARS[year]}
@@ -45,7 +45,7 @@ const Arrows = ({ children }) => {
     <div className="flex flex-row gap-2">
       <button
         onClick={() => paginateYear(false)}
-        className={`bg-accent bg-opacity-90 text-white
+        className={`bg-accent bg-opacity-70 text-white
           w-7 h-7 rounded
           transition duration-100 ease-linear
           hover:ring-1 hover:ring-accent hover:ring-offset-1`}
@@ -70,7 +70,7 @@ const Arrows = ({ children }) => {
 
       <button
         onClick={() => paginateYear(true)}
-        className={`bg-accent bg-opacity-90 text-white
+        className={`bg-accent bg-opacity-70 text-white
           w-7 h-7 rounded
           transition duration-100 ease-linear
           hover:ring-1 hover:ring-accent hover:ring-offset-1`}
@@ -114,11 +114,11 @@ const Button = ({ term, index }) => {
     <button
       onClick={() => paginateTerm(index)}
       className={` ${active ? "bg-accent text-white" : "bg-white text-accent"}
-      bg-opacity-90 font-medium 
-      border border-accent
+      bg-opacity-70 font-medium 
+      border border-accent border-opacity-85
       text-sm text-center rounded-sm w-7 h-7
       transition duration-100 ease-linear
-      hover:ring-1 hover:ring-accent hover:ring-offset-1
+      hover:ring-1 hover:ring-accent hover:ring-offset-1 hover:ring-opacity-85
       ${(thisYear || all) && "ring-1 ring-accent ring-offset-1"}`}
     >
       {term}
