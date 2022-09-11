6b812cbeec9732e72f0e46af35d46408742820f7
gn-grc <genejogarcia.gg@gmail.com>
2022-09-11 21:45:45 +0800

    added certs data

---
+++ src/screens/App/screens/Certifications/utils/data.js
@@ -0,0 +1,131 @@
+import coursera from "../../../../../shared/images/organizations/coursera.webp";
+import pirple from "../../../../../shared/images/organizations/pirple.webp";
+import udemy from "../../../../../shared/images/organizations/udemy.webp";
+import dict from "../../../../../shared/images/organizations/dict.webp";
+
+export default {
+  PIRPLE: {
+    NAME: "Pirple",
+    LOGO: pirple,
+
+    GRID_SPAN: "col-span-2 row-span-1",
+
+    CERTS: {
+      FSWDwF: {
+        TITLE: "Full Stack Web Development with Flask",
+        DATE_COMPLETED: "Jun 2021",
+        EXPIRATION_DATE: "No Expieration Date",
+        CREDENTIAL_ID: "MGXJL0FP9D",
+        CREDENTIAL_LINK: "https://www.pirple.com/certificates/mgxjl0fp9d",
+
+        SHORT_DESCRIPTION:
+          "Pellentesque congue augue sit amet felis posuere dictum.",
+      },
+    },
+  },
+
+  UDEMY: {
+    NAME: "Udemy",
+    LOGO: udemy,
+
+    GRID_SPAN: "col-span-2 row-span-1",
+
+    CERTS: {
+      CWDB: {
+        TITLE: "The Complete Web Development Bootcamp",
+        DATE_COMPLETED: "Jul 2021",
+        EXPIRATION_DATE: "No Expiration Date",
+        CREDENTIAL_ID: "UC-50ffd89e-dc35-48a4-9703-39d64655a0ac",
+        CREDENTIAL_LINK:
+          "https://www.udemy.com/certificate/UC-50ffd89e-dc35-48a4-9703-39d64655a0ac/",
+
+        SHORT_DESCRIPTION:
+          "Pellentesque congue augue sit amet felis posuere dictum.",
+      },
+    },
+  },
+
+  DICT: {
+    NAME: "Department of Information and Communications Technology",
+    LOGO: dict,
+
+    GRID_SPAN: "col-span-2 row-span-2",
+
+    CERTS: {
+      VDwP: {
+        TITLE: "Visualize Data with Python",
+        DATE_COMPLETED: "Jul 2022",
+        EXPIRATION_DATE: "No Expiration Date",
+        CREDENTIAL_ID: "66384089-1a8c-4a7a-ba8a-d31ff72edac1",
+        CREDENTIAL_LINK:
+          "https://courses.buri.io/view/user/certificate/66384089-1a8c-4a7a-ba8a-d31ff72edac1/pdf",
+
+        SHORT_DESCRIPTION:
+          "Pellentesque congue augue sit amet felis posuere dictum.",
+      },
+      LBSwP: {
+        TITLE: "Learn Basic Statistics with Python",
+        DATE_COMPLETED: "Jul 2022",
+        EXPIRATION_DATE: "No Expiration Date",
+        CREDENTIAL_ID: "f1b94a60-eb50-4f4e-887b-132eb7c6e668",
+        CREDENTIAL_LINK:
+          "https://courses.buri.io/view/user/certificate/f1b94a60-eb50-4f4e-887b-132eb7c6e668/pdf",
+
+        SHORT_DESCRIPTION:
+          "Pellentesque congue augue sit amet felis posuere dictum.",
+      },
+      ADwP: {
+        TITLE: "Analyze Data with Python",
+        DATE_COMPLETED: "Jul 2022",
+        EXPIRATION_DATE: "No Expiration Date",
+        CREDENTIAL_ID: "ef435030-414d-453d-a8bc-d4efbf77ff62",
+        CREDENTIAL_LINK:
+          "https://courses.buri.io/view/user/certificate/ef435030-414d-453d-a8bc-d4efbf77ff62/pdf",
+
+        SHORT_DESCRIPTION:
+          "Pellentesque congue augue sit amet felis posuere dictum.",
+      },
+    },
+  },
+
+  COURSERA: {
+    NAME: "Coursera",
+    LOGO: coursera,
+
+    GRID_SPAN: "col-span-2 row-span-2",
+
+    CERTS: {
+      OoDA: {
+        TITLE: "Overview of Data Analytics",
+        DATE_COMPLETED: "Jul 2022",
+        EXPIRATION_DATE: "No Expiration Date",
+        CREDENTIAL_ID: "NJJBP3VEKFGH",
+        CREDENTIAL_LINK:
+          "https://www.coursera.org/account/accomplishments/verify/NJJBP3VEKFGH",
+
+        SHORT_DESCRIPTION:
+          "Pellentesque congue augue sit amet felis posuere dictum.",
+      },
+      GSiGA: {
+        TITLE: "Getting Started in Google Analytics",
+        DATE_COMPLETED: "Aug 2022",
+        EXPIRATION_DATE: "No Expiration Date",
+        CREDENTIAL_ID: "9RVG36ZSMC73",
+        CREDENTIAL_LINK: "https://coursera.org/verify/9RVG36ZSMC73",
+
+        SHORT_DESCRIPTION:
+          "Pellentesque congue augue sit amet felis posuere dictum.",
+      },
+      EDAwS: {
+        TITLE: "Exploratory Data Analysis with Seaborn",
+        DATE_COMPLETED: "Aug 2022",
+        EXPIRATION_DATE: "No Expiration Date",
+        CREDENTIAL_ID: "Z64JDSNLVZJK",
+        CREDENTIAL_LINK: "https://coursera.org/verify/Z64JDSNLVZJK",
+
+        SHORT_DESCRIPTION:
+          "Pellentesque congue augue sit amet felis posuere dictum.",
+      },
+    },
+  },
+};
